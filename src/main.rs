53213ecd759b1085e80afab06028a50de2e7b436
Tommy Thorn <tommy-github2@thorn.ws>
2023-10-29 22:31:43 -0700

    Chasing the beam

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,55 @@
 use chrono::{self, Timelike};
-
 use minifb::{Key, Scale, ScaleMode, Window, WindowOptions};
-
-const WIDTH: usize = 640;
-const HEIGHT: usize = 480;
 
 type Point2D = (i32, i32);
 
 // Triangle coordiantes *must* be presenting clockwise
 type Triangle = [Point2D; 3];
 type Color = u32;
-type Fb = [Color; WIDTH * HEIGHT];
 
-struct Tile {
-    x0: i32,
-    y0: i32,
-    x1: i32,
-    y1: i32,
+const WIDTH: usize = 640;
+const HEIGHT: usize = 480;
+
+struct RenderTile {
     a: [i32; 3],
     b: [i32; 3],
     c: [i32; 3],
+
+    e0: [i32; 3],
+    e: [i32; 3],
+}
+
+impl RenderTile {
+    fn new(a: [i32; 3], b: [i32; 3], c: [i32; 3]) -> Self {
+        Self {
+            a,
+            b,
+            c,
+            e0: c,
+            e: c,
+        }
+    }
+
+    fn start(&mut self) {
+        self.e0 = self.c;
+        self.e = self.c
+    }
+
+    fn stepx(&mut self, pixel: &mut Color, color: Color) {
+        if 0 <= self.e[0] && 0 <= self.e[1] && 0 <= self.e[2] {
+            *pixel = color;
+        }
+        for i in 0..3 {
+            self.e[i] += self.a[i];
+        }
+    }
+
+    fn stepy(&mut self) {
+        for i in 0..3 {
+            self.e0[i] += self.b[i];
+            self.e = self.e0;
+        }
+    }
 }
 
 /* Assume the points are homogeneous */
@@ -31,106 +61,20 @@ fn edge_equation(v0: &Point2D, v1: &Point2D) -> [i32; 3] {
     [a, b, c]
 }
 
-fn rasterize_tile(fb: &mut Fb, tile: Tile, color: Color) {
-    let mut e0 = [0; 3];
-    for i in 0..3 {
-        e0[i] = tile.a[i] * tile.x0 + tile.b[i] * tile.y0 + tile.c[i];
-    }
-
-    for y in tile.y0..=tile.y1 {
-        let mut e = e0;
-
-        for x in tile.x0..=tile.x1 {
-            if 0 <= e[0]
-                && 0 <= e[1]
-                && 0 <= e[2]
-                && 0 <= y
-                && y < HEIGHT as i32
-                && 0 <= x
-                && x < WIDTH as i32
-            {
-                assert!(0 <= y && y < HEIGHT as i32, "({x},{y}) outside screen");
-                assert!(0 <= x && x < WIDTH as i32, "({x},{y}) outside screen");
-                fb[y as usize * WIDTH + x as usize] = color;
-            }
-            for i in 0..3 {
-                e[i] += tile.a[i];
-            }
-        }
-
-        for i in 0..3 {
-            e0[i] += tile.b[i];
-        }
-    }
-}
-
-fn rasterize_triangle(fb: &mut Fb, v: Triangle, color: Color) {
+fn rasterize_triangle(v: Triangle) -> RenderTile {
     let mut a = [0; 3];
     let mut b = [0; 3];
     let mut c = [0; 3];
-
-    let mut x0 = v[0].0;
-    let mut x1 = v[0].0;
-    let mut y0 = v[0].1;
-    let mut y1 = v[0].1;
 
     for i in 0..3 {
         let edge = edge_equation(&v[i], &v[(i + 1) % 3]);
         a[i] = edge[0];
         b[i] = edge[1];
         c[i] = edge[2];
-        x0 = v[i].0.min(x0);
-        x1 = v[i].0.max(x1);
-        y0 = v[i].1.min(y0);
-        y1 = v[i].1.max(y1);
     }
 
-    //println!("({x0},{y0}) - ({x1},{y1})");
-
-    let tile = Tile {
-        x0,
-        y0,
-        x1,
-        y1,
-        a,
-        b,
-        c,
-    };
-
-    rasterize_tile(fb, tile, color);
+    RenderTile::new(a, b, c)
 }
-
-//                     (20, 0)
-//                     #
-//                    ###
-//                  #######
-//                ###########
-//               ##############
-//             ##################
-//           ######################
-//          #########################
-//        #############################
-//      ################################
-//    ##(3,10)############################
-//            ##############################
-//                    ########################
-//                            ##################
-//                                    ############
-//                                            ###### (48, 15)
-//
-
-/*
-fn lastnonspace(line: &[char]) -> usize {
-    let mut last = 0;
-    for (i, ch) in line.iter().enumerate() {
-        if *ch != ' ' {
-            last = i + 1;
-        }
-    }
-
-    last
-}
-*/
 
 fn rotate(p: Point2D, origin: Point2D, angle: f32) -> Point2D {
     let (x, y) = (p.0 - origin.0, p.1 - origin.1);
@@ -167,40 +111,33 @@ fn main() {
     let p1 = (center.0 + 20, center.1 + 20);
     let p2 = (center.0 - 20, center.1 + 20);
 
-    //println!("{:?}", chrono::offset::Local::now());
-
     while window.is_open() && !window.is_key_down(Key::Q) {
         let t = chrono::offset::Local::now();
         let (h, m, s) = (t.hour() % 12, t.minute(), t.second());
 
-        let hour_rad = 3.1415926535 * 2. / 12. * h as f32;
-        let minute_rad = 3.1415926535 * 2. / 60. * m as f32;
-        let second_rad = 3.1415926535 * 2. / 60. * s as f32;
+        let hour_rad = std::f32::consts::PI * 2. / 12. * h as f32;
+        let minute_rad = std::f32::consts::PI * 2. / 60. * m as f32;
+        let second_rad = std::f32::consts::PI * 2. / 60. * s as f32;
 
-        rasterize_triangle(
-            &mut fb,
+        let mut hour_tile = rasterize_triangle(
             [hp0, p1, p2]
                 .iter()
                 .map(|p| rotate(*p, center, hour_rad))
                 .collect::<Vec<_>>()
                 .try_into()
                 .unwrap(),
-            0xFF0000,
         );
 
-        rasterize_triangle(
-            &mut fb,
+        let mut minute_tile = rasterize_triangle(
             [mp0, p1, p2]
                 .iter()
                 .map(|p| rotate(*p, center, minute_rad))
                 .collect::<Vec<_>>()
                 .try_into()
                 .unwrap(),
-            0xFFFF00,
         );
 
-        rasterize_triangle(
-            &mut fb,
+        let mut second_tile = rasterize_triangle(
             [
                 (center.0, 3),
                 (center.0 + 3, center.1 - 3),
@@ -211,8 +148,23 @@ fn main() {
             .collect::<Vec<_>>()
             .try_into()
             .unwrap(),
-            0xFFFFFF,
         );
+
+        hour_tile.start();
+        minute_tile.start();
+        second_tile.start();
+
+        for y in 0..HEIGHT {
+            for x in 0..WIDTH {
+                let pixel = &mut fb[x + y * WIDTH];
+                hour_tile.stepx(pixel, 0xFF0000);
+                minute_tile.stepx(pixel, 0xFFFF00);
+                second_tile.stepx(pixel, 0xFFFFFF);
+            }
+            hour_tile.stepy();
+            minute_tile.stepy();
+            second_tile.stepy();
+        }
 
         window.update_with_buffer(&fb, WIDTH, HEIGHT).unwrap();
         fb = [0; HEIGHT * WIDTH];
